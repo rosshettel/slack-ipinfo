66456e3470d553dde9271009929920ad50def8ac
Ross Hettel <rosshettel@gmail.com>
2015-11-22 21:58:30 -0600

    added slack api

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 'use strict';
 var express = require('express'),
     bodyParser = require('body-parser'),
+    slack = require('slack-api'),
     app = express(),
     port = process.env.PORT || 3000,
-    server = app.listen(port);
+    server = app.listen(port),;
 
 const TOKEN = process.env.TOKEN;
 const WEBHOOK = process.env.WEBHOOK;
@@ -16,9 +17,21 @@ function isValidToken(payload) {
 }
 
 app.get('/oauth', function (req, res) {
-    //get query param `code`
-    //
-})
+    if (!req.query.code) {
+        res.status(400).send('Code query param not found!');
+    }
+
+    slack.oauth.access({
+        client_id: process.env.CLIENT_ID,
+        client_scret: process.env.CLIENT_SECRET,
+        code: req.query.code
+    }, function (err, data) {
+        if (err) {
+            console.log('err');
+        }
+        console.log('data', data);
+    });
+});
 
 app.post('/', function (req, res) {
     let payload = req.body,
