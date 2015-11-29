3c5a053302a6e7c67ce066f5cbcfe89feef5029a
Ross Hettel <rosshettel@gmail.com>
2015-11-28 23:40:17 -0600

    logging

--- nmap.js
+++ nmap.js
@@ -10,12 +10,16 @@ var Nmap = function () {
 
     function validInput(input) {
         if (validator.isIP(input)) {
+            logger.info('isIP true');
             return true;
         } else {
+            logger.info('isIP false');
             dns.lookup(input, function (err, addresses, family) {
                 if (err) {
+                    logger.info('dns lookup err', err);
                     return false;
                 } else {
+                    logger.info('dns lookup', addresses);
                     return true;
                 }
             });
